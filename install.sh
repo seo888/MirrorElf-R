1ed6d8f68d8e333b52722c7b7b9f17a81e45e91b
Seo <53137698+seo888@users.noreply.github.com>
2025-02-17 10:05:00 +0000

    update

--- install.sh
+++ install.sh
@@ -65,5 +65,17 @@ PROJECT_DIR="/www/MirrorElf"
 # 切换到项目目录
 cd "$PROJECT_DIR" || exit 1
 
+
+# 定义输出文件
+IPS_TXT="./config/IPS.txt"
+
+# 获取所有 IP 地址并排除 127.0.0.1 和局域网 IP 地址
+ips=$(ip addr | grep 'inet ' | awk '{print $2}' | cut -d/ -f1 | \
+    grep -v '^127.0.0.1$' | \
+    grep -vE '^10\.|^172\.(1[6-9]|2[0-9]|3[0-1])\.|^192\.168\.')
+    
+# 将 IP 地址写入文件
+echo "$ips" > "$IPS_TXT"
+
 # 启动容器
 docker compose up -d || exit 1
